435a7e0c6e5d14d9b43d7b0ec4ae39593dce7a7b
mariellewardenaar <mariellewardenaar@outlook.com>
2018-02-12 14:34:44 +0100

    init commit

---
+++ Opdracht1/js/script.js
@@ -0,0 +1,54 @@
+
+function myFunction1() {
+  var style = document.getElementById('open1').style;
+  if( style.display == "none" ) {
+    style.display = "block";
+  } else {
+    style.display = "none";
+  }
+}
+
+function myFunction2() {
+  var style = document.getElementById('open2').style;
+  if( style.display == "none" ) {
+    style.display = "block";
+  } else {
+    style.display = "none";
+  }
+}
+
+function myFunction3() {
+  var style = document.getElementById('open3').style;
+  if( style.display == "none" ) {
+    style.display = "block";
+  } else {
+    style.display = "none";
+  }
+}
+
+function myFunction4() {
+  var style = document.getElementById('open4').style;
+  if( style.display == "none" ) {
+    style.display = "block";
+  } else {
+    style.display = "none";
+  }
+}
+
+function myFunction5() {
+  var style = document.getElementById('open5').style;
+  if( style.display == "none" ) {
+    style.display = "block";
+  } else {
+    style.display = "none";
+  }
+}
+
+function myFunction6() {
+  var style = document.getElementById('open6').style;
+  if( style.display == "none" ) {
+    style.display = "block";
+  } else {
+    style.display = "none";
+  }
+}
