5963f92dad86d5d6935dba07620758fc90354448
mariellewardenaar <36385239+mariellewardenaar@users.noreply.github.com>
2018-02-20 08:33:02 +0100

    Update script.js

--- Opdracht1/js/script.js
+++ Opdracht1/js/script.js
@@ -1,54 +1,198 @@
+//eerste button
 
-function myFunction1() {
-  var style = document.getElementById('open1').style;
-  if( style.display == "none" ) {
-    style.display = "block";
-  } else {
-    style.display = "none";
-  }
-}
 
-function myFunction2() {
-  var style = document.getElementById('open2').style;
-  if( style.display == "none" ) {
-    style.display = "block";
-  } else {
-    style.display = "none";
-  }
-}
+// om de meer informatie te togglen
 
-function myFunction3() {
-  var style = document.getElementById('open3').style;
-  if( style.display == "none" ) {
-    style.display = "block";
-  } else {
-    style.display = "none";
-  }
-}
+var klikbaar = document.querySelector('#info');
+var open = document.querySelector('.open');
+//console.log("klikbaar " + klikbaar)
 
-function myFunction4() {
-  var style = document.getElementById('open4').style;
-  if( style.display == "none" ) {
-    style.display = "block";
-  } else {
-    style.display = "none";
-  }
-}
 
-function myFunction5() {
-  var style = document.getElementById('open5').style;
-  if( style.display == "none" ) {
-    style.display = "block";
-  } else {
-    style.display = "none";
+klikbaar.addEventListener("click", function(event){
+    open.classList.toggle('open');
   }
-}
+);
 
-function myFunction6() {
-  var style = document.getElementById('open6').style;
-  if( style.display == "none" ) {
-    style.display = "block";
-  } else {
-    style.display = "none";
+// om de button in te laten bouncen
+
+var transitie = document.getElementById("favcount");
+
+transitie.classList.toggle("bounceIn");
+
+
+//om de tekst meer informatie te veranderen
+
+var informatie = document.querySelector('#info')
+
+informatie.addEventListener("click",function(){     
+	document.getElementById("info").textContent="minder informatie"; } );
+
+//
+
+
+
+
+
+
+
+
+//tweede button
+
+// om de meer informatie te togglen
+
+var klikbaar2 = document.querySelector('#info2');
+var open2 = document.querySelector('.open1');
+//console.log("klikbaar " + klikbaar)
+
+
+klikbaar2.addEventListener("click", function(event){
+    open2.classList.toggle('open1');
   }
-}
+);
+
+// om de button in te laten bouncen
+
+var transitie2 = document.getElementById("favcount2");
+
+transitie2.classList.toggle("bounceIn");
+
+//om de tekst meer informatie te veranderen
+
+var informatie = document.querySelector('#info2')
+
+informatie.addEventListener("click",function(){     
+	document.getElementById("info2").textContent="minder informatie"; } );
+
+
+
+
+
+
+
+
+
+
+//derde button
+
+// om de meer informatie te togglen
+
+var klikbaar3 = document.querySelector('#info3');
+var open3 = document.querySelector('.open2');
+//console.log("klikbaar " + klikbaar)
+
+
+klikbaar3.addEventListener("click", function(event){
+    open3.classList.toggle('open2');
+  }
+);
+
+// om de button in te laten bouncen
+
+var transitie3 = document.getElementById("favcount3");
+
+transitie3.classList.toggle("bounceIn");
+
+//om de tekst meer informatie te veranderen
+
+var informatie = document.querySelector('#info3')
+
+informatie.addEventListener("click",function(){     
+	document.getElementById("info3").textContent="minder informatie"; } );
+
+
+
+
+
+
+
+//vierde button
+
+// om de meer informatie te togglen
+
+var klikbaar4 = document.querySelector('#info4');
+var open4 = document.querySelector('.open3');
+//console.log("klikbaar " + klikbaar)
+
+
+klikbaar4.addEventListener("click", function(event){
+    open4.classList.toggle('open3');
+  }
+);
+
+// om de button in te laten bouncen
+
+var transitie4 = document.getElementById("favcount4");
+
+transitie4.classList.toggle("bounceIn");
+
+//om de tekst meer informatie te veranderen
+
+var informatie = document.querySelector('#info4')
+
+informatie.addEventListener("click",function(){     
+	document.getElementById("info4").textContent="minder informatie"; } );
+
+
+
+
+
+
+
+//vijfde button
+
+// om de meer informatie te togglen
+
+var klikbaar5 = document.querySelector('#info5');
+var open5 = document.querySelector('.open4');
+//console.log("klikbaar " + klikbaar)
+
+
+klikbaar5.addEventListener("click", function(event){
+    open5.classList.toggle('open4');
+  }
+);
+
+// om de button in te laten bouncen
+
+var transitie5 = document.getElementById("favcount5");
+
+transitie5.classList.toggle("bounceIn");
+
+//om de tekst meer informatie te veranderen
+
+var informatie = document.querySelector('#info5')
+
+informatie.addEventListener("click",function(){     
+	document.getElementById("info5").textContent="minder informatie"; } );
+
+
+
+
+
+//zesde button
+
+// om de meer informatie te togglen
+
+var klikbaar6 = document.querySelector('#info6');
+var open6 = document.querySelector('.open5');
+//console.log("klikbaar " + klikbaar)
+
+
+klikbaar6.addEventListener("click", function(event){
+    open6.classList.toggle('open5');
+  }
+);
+
+// om de button in te laten bouncen
+
+var transitie6 = document.getElementById("favcount6");
+
+transitie6.classList.toggle("bounceIn");
+
+//om de tekst meer informatie te veranderen
+
+var informatie = document.querySelector('#info6')
+
+informatie.addEventListener("click",function(){     
+	document.getElementById("info6").textContent="minder informatie"; } );
+
