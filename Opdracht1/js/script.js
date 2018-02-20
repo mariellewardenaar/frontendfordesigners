6436e18ac7e0181194a4fbb15a9ef1ed1e00f8d6
mariellewardenaar <36385239+mariellewardenaar@users.noreply.github.com>
2018-02-20 15:14:59 +0100

    Update script.js

--- Opdracht1/js/script.js
+++ Opdracht1/js/script.js
@@ -6,28 +6,29 @@
 var klikbaar = document.querySelector('#info');
 var open = document.querySelector('.open');
 //console.log("klikbaar " + klikbaar)
+var transitie = document.getElementById("favcount");
 
 
 klikbaar.addEventListener("click", function(event){
     open.classList.toggle('open');
+    transitie.classList.toggle('bounceIn');
   }
 );
-
-// om de button in te laten bouncen
-
-var transitie = document.getElementById("favcount");
-
-transitie.classList.toggle("bounceIn");
 
 
 //om de tekst meer informatie te veranderen
 
-var informatie = document.querySelector('#info')
+var informatie = document.querySelector('#info');
 
 informatie.addEventListener("click",function(){     
-	document.getElementById("info").textContent="minder informatie"; } );
-
-//
+	var infobox = document.getElementById("info");
+  
+  if(infobox.textContent=="minder informatie"){
+    infobox.textContent="meer informatie"; 
+  } else{
+    infobox.textContent="minder informatie"; 
+  }
+} );
 
 
 
@@ -38,30 +39,34 @@ informatie.addEventListener("click",function(){
 
 //tweede button
 
+
 // om de meer informatie te togglen
 
 var klikbaar2 = document.querySelector('#info2');
 var open2 = document.querySelector('.open1');
 //console.log("klikbaar " + klikbaar)
+var transitie2 = document.getElementById("favcount2");
 
 
 klikbaar2.addEventListener("click", function(event){
     open2.classList.toggle('open1');
+    transitie2.classList.toggle('bounceIn');
   }
 );
 
-// om de button in te laten bouncen
-
-var transitie2 = document.getElementById("favcount2");
-
-transitie2.classList.toggle("bounceIn");
-
 //om de tekst meer informatie te veranderen
 
-var informatie = document.querySelector('#info2')
+var informatie = document.querySelector('#info2');
 
 informatie.addEventListener("click",function(){     
-	document.getElementById("info2").textContent="minder informatie"; } );
+  var infobox = document.getElementById("info2");
+  
+  if(infobox.textContent=="minder informatie"){
+    infobox.textContent="meer informatie"; 
+  } else{
+    infobox.textContent="minder informatie"; 
+  }
+} );
 
 
 
@@ -74,30 +79,33 @@ informatie.addEventListener("click",function(){
 
 //derde button
 
+
 // om de meer informatie te togglen
 
 var klikbaar3 = document.querySelector('#info3');
 var open3 = document.querySelector('.open2');
 //console.log("klikbaar " + klikbaar)
+var transitie3 = document.getElementById("favcount3");
 
 
 klikbaar3.addEventListener("click", function(event){
     open3.classList.toggle('open2');
+    transitie3.classList.toggle('bounceIn');
   }
 );
-
-// om de button in te laten bouncen
-
-var transitie3 = document.getElementById("favcount3");
-
-transitie3.classList.toggle("bounceIn");
-
 //om de tekst meer informatie te veranderen
 
-var informatie = document.querySelector('#info3')
+var informatie = document.querySelector('#info3');
 
 informatie.addEventListener("click",function(){     
-	document.getElementById("info3").textContent="minder informatie"; } );
+  var infobox = document.getElementById("info3");
+  
+  if(infobox.textContent=="minder informatie"){
+    infobox.textContent="meer informatie"; 
+  } else{
+    infobox.textContent="minder informatie"; 
+  }
+} );
 
 
 
@@ -112,26 +120,28 @@ informatie.addEventListener("click",function(){
 var klikbaar4 = document.querySelector('#info4');
 var open4 = document.querySelector('.open3');
 //console.log("klikbaar " + klikbaar)
+var transitie4 = document.getElementById("favcount4");
 
 
 klikbaar4.addEventListener("click", function(event){
     open4.classList.toggle('open3');
+    transitie4.classList.toggle('bounceIn');
   }
 );
 
-// om de button in te laten bouncen
-
-var transitie4 = document.getElementById("favcount4");
-
-transitie4.classList.toggle("bounceIn");
-
 //om de tekst meer informatie te veranderen
 
-var informatie = document.querySelector('#info4')
+var informatie = document.querySelector('#info4');
 
 informatie.addEventListener("click",function(){     
-	document.getElementById("info4").textContent="minder informatie"; } );
-
+  var infobox = document.getElementById("info4");
+  
+  if(infobox.textContent=="minder informatie"){
+    infobox.textContent="meer informatie"; 
+  } else{
+    infobox.textContent="minder informatie"; 
+  }
+} );
 
 
 
@@ -145,25 +155,28 @@ informatie.addEventListener("click",function(){
 var klikbaar5 = document.querySelector('#info5');
 var open5 = document.querySelector('.open4');
 //console.log("klikbaar " + klikbaar)
+var transitie5 = document.getElementById("favcount5");
 
 
 klikbaar5.addEventListener("click", function(event){
     open5.classList.toggle('open4');
+    transitie5.classList.toggle('bounceIn');
   }
 );
 
-// om de button in te laten bouncen
-
-var transitie5 = document.getElementById("favcount5");
-
-transitie5.classList.toggle("bounceIn");
-
 //om de tekst meer informatie te veranderen
 
-var informatie = document.querySelector('#info5')
+var informatie = document.querySelector('#info5');
 
 informatie.addEventListener("click",function(){     
-	document.getElementById("info5").textContent="minder informatie"; } );
+  var infobox = document.getElementById("info5");
+  
+  if(infobox.textContent=="minder informatie"){
+    infobox.textContent="meer informatie"; 
+  } else{
+    infobox.textContent="minder informatie"; 
+  }
+} );
 
 
 
@@ -176,23 +189,25 @@ informatie.addEventListener("click",function(){
 var klikbaar6 = document.querySelector('#info6');
 var open6 = document.querySelector('.open5');
 //console.log("klikbaar " + klikbaar)
+var transitie6 = document.getElementById("favcount6");
 
 
 klikbaar6.addEventListener("click", function(event){
     open6.classList.toggle('open5');
+    transitie6.classList.toggle('bounceIn');
   }
 );
 
-// om de button in te laten bouncen
-
-var transitie6 = document.getElementById("favcount6");
-
-transitie6.classList.toggle("bounceIn");
-
 //om de tekst meer informatie te veranderen
 
-var informatie = document.querySelector('#info6')
+var informatie = document.querySelector('#info6');
 
 informatie.addEventListener("click",function(){     
-	document.getElementById("info6").textContent="minder informatie"; } );
-
+  var infobox = document.getElementById("info6");
+  
+  if(infobox.textContent=="minder informatie"){
+    infobox.textContent="meer informatie"; 
+  } else{
+    infobox.textContent="minder informatie"; 
+  }
+} );
